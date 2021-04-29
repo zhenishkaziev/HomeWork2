7fde52e635387473f840c1a2c2fcff0feb18b372
zhenishkaziev <zhenishkaziev@gmail.com>
2021-04-29 15:47:47 +0300

    Обновление

--- app/src/main/java/com/example/homework2/SecondNeedActivity.java
+++ app/src/main/java/com/example/homework2/SecondNeedActivity.java
@@ -11,42 +11,61 @@ import android.widget.Button;
 import android.widget.ImageView;
 
 public class SecondNeedActivity extends AppCompatActivity {
+    ImageView imPhoto;
+    Button bntOpen, bntCamera, bntGallery;
+    int RECUECT = 1;
+    Uri photo;
 
-       Button bntOpen, bntCamera, bntGallery;
-       
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_need);
         bntOpen = findViewById(R.id.chrome_bnt);
         bntCamera = findViewById(R.id.camera_bnt);
-        bntGallery =  findViewById(R.id.gallery_bnt);
+        bntGallery = findViewById(R.id.gallery_bnt);
 
 
         bntOpen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                   Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
-                   startActivity(intent);
+                Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
+                startActivity(intent);
             }
         });
 
         bntCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                    Intent camera = new Intent(Intent.ACTION_CAMERA_BUTTON);
-                    startActivity(camera);
+                Intent camera = new Intent("android.media.action.IMAGE_CAPTURE");
+                startActivity(camera);
             }
         });
 
-           bntGallery.setOnClickListener(new View.OnClickListener() {
-               @Override
-               public void onClick(View v) {
-                   Intent gallery = new Intent(Intent.CATEGORY_APP_GALLERY);
-                   startActivity(gallery);
-               }
-           });
-
+        bntGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+                startActivity(gallery);
+            }
+        });
+//         imPhoto.setOnClickListener(new View.OnClickListener() {
+//             @Override
+//             public void onClick(View v) {
+//
+//                 Intent imageGetter = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+//                 startActivityForResult(imageGetter, RECUECT);
+//             }
+//         });
+//        }
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+//    {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if(requestCode == RECUECT && resultCode == RESULT_OK && null != data);
+//          photo =  data.getData();
+//          imPhoto.setImageURI(photo);
+//       }
+//    }
 
 
     }
