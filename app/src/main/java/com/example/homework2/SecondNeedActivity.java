20f193adb77c97d8c8c18c2ec5a5180047048f56
zhenishkaziev <zhenishkaziev@gmail.com>
2021-04-29 17:15:32 +0300

    New proekt

--- app/src/main/java/com/example/homework2/SecondNeedActivity.java
+++ app/src/main/java/com/example/homework2/SecondNeedActivity.java
@@ -3,18 +3,25 @@ package com.example.homework2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.InputStream;
+import java.io.InputStream;import java.io.InputStream;
 
 public class SecondNeedActivity extends AppCompatActivity {
-    ImageView imPhoto;
     Button bntOpen, bntCamera, bntGallery;
-    int RECUECT = 1;
-    Uri photo;
+     final int RECUST = 1;
+    ImageView imPhoto;
+    Uri uri;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +30,7 @@ public class SecondNeedActivity extends AppCompatActivity {
         bntOpen = findViewById(R.id.chrome_bnt);
         bntCamera = findViewById(R.id.camera_bnt);
         bntGallery = findViewById(R.id.gallery_bnt);
+        imPhoto = findViewById(R.id.photo_im);
 
 
         bntOpen.setOnClickListener(new View.OnClickListener() {
@@ -44,10 +52,32 @@ public class SecondNeedActivity extends AppCompatActivity {
         bntGallery.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
-                startActivity(gallery);
+                Intent gallery = new Intent(Intent.ACTION_PICK);
+                gallery.setType("image/*");
+                startActivityForResult(gallery, RECUST);
             }
         });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+
+        switch (requestCode){
+            case RECUST:
+        if ( resultCode == RESULT_OK){
+            try {
+                final Uri imageUri = imageReturnedIntent.getData();
+                final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                imPhoto.setImageBitmap(selectedImage);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        }}}
+
+
+
 //         imPhoto.setOnClickListener(new View.OnClickListener() {
 //             @Override
 //             public void onClick(View v) {
@@ -68,5 +98,3 @@ public class SecondNeedActivity extends AppCompatActivity {
 //    }
 
 
-    }
-}
